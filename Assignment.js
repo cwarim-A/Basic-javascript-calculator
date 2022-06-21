bf55caaf850c7514be6845a74addb3b3687e3086
Lanrey-dev <63509122+cwarim-A@users.noreply.github.com>
2022-06-21 11:21:49 +0100

    Add files via upload

---
+++ Assignment.js
@@ -0,0 +1,24 @@
+// program for a simple calculator
+
+// take the operator input
+const operator = prompt("Enter operator ( either +, -, * or / ): ");
+
+// take the operand input
+const firstNumber = parseFloat(prompt("Enter first number: "));
+const secondNumber = parseFloat(prompt("Enter second number: "));
+
+let result;
+
+// using if...else if... else
+if (operator == "+") {
+  result = firstNumber + secondNumber;
+} else if (operator == "-") {
+  result = firstNumber - secondNumber;
+} else if (operator == "*") {
+  result = firstNumber * secondNumber;
+} else {
+  result = firstNumber / secondNumber;
+}
+
+// display the result
+console.log(`${firstNumber} ${operator} ${secondNumber} = ${result}`);
